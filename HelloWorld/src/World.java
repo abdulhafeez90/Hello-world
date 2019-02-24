4fe0a43ed2fe1c4bc0471809bb56d68a1ec15b8f
Abdul Hafeez <Abdul Hafeez@192.168.1.5>
2019-02-24 16:18:17 +0530

    First commit
    uploading all files...

---
+++ HelloWorld/src/World.java
@@ -0,0 +1,16 @@
+class World 
+{
+	//constructor
+	World()
+	{
+		int i,j;
+		i = 5;
+		j = 3;
+		int add = i+j;
+		System.out.println("The sum of 2 given numbers is "+add);
+	}
+	public static void main(String[] args)
+	{
+		new World();
+	}
+}
